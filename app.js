47766746e8d6e2890c005dbd4d70059f67dcb428
iReiki <jeanluc.lys@gmail.com>
2023-10-10 12:07:30 +0200

    update header access

--- app.js
+++ app.js
@@ -21,6 +21,19 @@ const commentRoutes = require('./routes/comment_route');
 const invoiceRoutes = require('./routes/invoice_route');
 const buyRoutes = require('./routes/buy_route');
 
+app.use(function (req, res, next) {
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    //res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
+
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/subscriptions', subscriptionRoutes);
